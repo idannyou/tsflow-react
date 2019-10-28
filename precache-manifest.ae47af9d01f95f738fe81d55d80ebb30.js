38daf209e445ed7ce9026c1a6c68b369d42818ee
Danny Ou <idannyou@gmail.com>
2019-10-27 23:12:07 -0700

    Updates

---
+++ precache-manifest.ae47af9d01f95f738fe81d55d80ebb30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79cb935a2a51d1c3d79751f43aa05eba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10ddde1e9ec6fbaf54c3",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5e9ba2a4529d3911eee1",
+    "url": "/static/js/2.ebdd5bec.chunk.js"
+  },
+  {
+    "revision": "10ddde1e9ec6fbaf54c3",
+    "url": "/static/js/main.8157e05f.chunk.js"
+  },
+  {
+    "revision": "9d6755eaba5348f23f30",
+    "url": "/static/js/runtime-main.9cd61db0.js"
+  }
+]);
